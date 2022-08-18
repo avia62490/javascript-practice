3eeb1012f322165722664278b254d7e896eefae4
Alex Via <alexandervia@gmail.com>
2022-08-18 18:22:49 -0500

    Completes loop methods

--- loops.js
+++ loops.js
@@ -1,22 +1,76 @@
 // # 1. Write a while loop to print the numbers 1 through 10.
+// var index = 1;
+// while (index <= 10) {
+//   console.log(index);
+//   index ++
+// }
 
 // # 2. Write a while loop that prints the word "hello" 5 times.
+// var index = 1;
+// while (index < 6) {
+//   console.log("hello");
+//   index ++
+// }
 
 // # 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
+const prompt = require('prompt-sync')();
+// while (true) {
+//   var entry = prompt("enter a word: ");
+//   if (entry === "stop") {
+//     break;
+//   }
+// }
 
 // # 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+// var index = 0
+// while (index <= 100) {
+//   console.log(index);
+//   index += 5;
+// }
 
 // # 5. Write a while loop that prints the number 9000 ten times.
+// var number = 9000;
+// index = 0;
+// while (index < 10) {
+//   console.log(number);
+//   index ++
+// }
 
 // # 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
+// while (true) {
+//   var number = prompt("Enter a number: ");
+//   if (number > 10) {
+//     break;
+//   }
+// }
 
 // # 7. Write a while loop that prints the numbers 50 to 70.
+// var index = 50;
+// while(index <= 70) {
+//   console.log(index);
+//   index ++
+// }
 
 // # 8. Write a while loop that prints the phrase "Around the world" 144 times.
+// index = 0;
+// while (index < 144) {
+//   console.log("Around the world");
+//   index ++
+// }
 
 // # 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
+// while (true) {
+//   var word = prompt("enter a word: ")
+//   if (word.length > 5) {
+//     break;
+//   }
+// }
 
 // # 10. Write a while loop that prints the even numbers from 2 to 40.
-
+var index = 2;
+while (index <= 40) {
+  console.log(index);
+  index += 2
+}
 
 // # SOLUTIONS: https://gist.github.com/peterxjang/c4ec0e0f8f6e123d65ada9bf3100068b
